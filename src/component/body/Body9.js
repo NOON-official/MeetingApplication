f30c833413983c50444dec8b7873dd945fc32434
GyuhaWang <ethanleast@gmail.com>
2022-08-11 20:29:41 +0900

    디자인 적용 70%

--- src/component/body/Body9.js
+++ src/component/body/Body9.js
@@ -1,117 +1,46 @@
-import styled from 'styled-components'
-import * as React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-const buttonColor = '#218fc3'
-const Container = styled.div`
-  left: 0;
-  overflow: hidden;
-  overflow-x: hidden;
-  position: absolute;
-  top: 10%;
-  width: 100%;
-  height: 75%;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
+import Wants from '../SelectWants/Wants'
 
-const Title = styled.div`
-  display: flex;
-  top: 2%;
-  font-size: 17px;
-  font-weight: bold;
-  height: auto;
-  margin: 0;
-  color: #000;
-  font-style: normal;
-  letter-spacing: -0.015em;
-  line-height: 149.8%;
-  position: absolute;
-  text-align: center;
-  justify-content: center;
-  width: auto;
-`
-const SubTitle = styled.div`
-  display: flex;
-  top: ${(props) => props.top || '10%'};
-  margin-top: ${(props) => props.margin_top};
-  font-size: 12px;
-  font-weight: 150;
-  height: 27px;
-  margin-left: ${(props) => props.margin_left};
-  color: #000;
-  font-style: normal;
-  letter-spacing: -0.015em;
-  line-height: 149.8%;
-  position: absolute;
-  text-align: center;
-  justify-content: center;
-  width: auto;
-`
-const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 60%;
+import { ReactComponent as Character } from '../../Asset/page9/Character.svg'
+import {
+  Container,
+  MobileBox,
+  StyledDiv,
+  StyledText,
+  SubTitle,
+} from '../Elements/StyledComponent'
 
-  margin: 10px;
-  width: 301px;
-  overflow: hidden;
-  overflow-x: hidden;
-`
-const TextBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 40%;
-  border: 2px solid #c9c9c9;
-  border-radius: 12px;
-  box-sizing: border-box;
-  margin: 10px;
-  width: 295px;
-  top: 32%;
-  position: absolute;
-  overflow: hidden;
-  overflow-x: hidden;
-`
-const IntroBox = styled.textarea`
-  position: absolute;
-  top: 10%;
-  width: 98%;
-  height: 80%;
-  border: 0;
-  ::placeholder {
-    justify-content: flex-start;
-    text-align: start;
-    font-size: 13px;
-  }
-`
 const Body9 = () => {
-  const dispatch = useDispatch()
-  const setIntroduction = (e) => {
-    dispatch({ type: 'SET_INTRODUCTION', payload: e.target.value })
-  }
-
   return (
     <Container>
-      <Title>우리팀 어필할 수 있는 마지막 소개 한줄</Title>
-      <SubTitle margin_top="25px">
-        위트있게 우리팀을 두문장 이내로 설명해보아요!
-      </SubTitle>
-      <TextContainer>
-        <SubTitle margin_left="15px" top="30%">
-          2문장 이내로 우리팀을 소개해 주세요!
+      <MobileBox>
+        <StyledDiv top="5%" width="90%" height="20%" left="50%">
+          <StyledText
+            position=" absolute"
+            size="35px"
+            color="#F49393"
+            left="3%"
+          >
+            그날의 분위기
+          </StyledText>
+          <StyledText position="absolute" size="35px" left="15%">
+            는 <br /> 어땠으면 좋겠어요?
+          </StyledText>
+        </StyledDiv>{' '}
+        <SubTitle
+          size="13px"
+          font="Pretendard"
+          color="#AAAAAA"
+          left="23%"
+          top="20%"
+        >
+          중복 선택이 가능해요
         </SubTitle>
-
-        <TextBox>
-          <IntroBox
-            onChange={setIntroduction}
-            placeholder="안녕하세요 한국외국어대학교 개그동아리 학생들입니다. 진짜 재미를 원하는 분들은 후회하지 않을 겁니다^^ 아, 참고로 잘생겼습니다."
-          ></IntroBox>
-        </TextBox>
-      </TextContainer>
+        <Wants></Wants>
+        <StyledDiv top="82%" left="75%">
+          <Character />
+        </StyledDiv>
+      </MobileBox>
     </Container>
   )
 }
-
 export default Body9
