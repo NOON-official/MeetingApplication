76fe5fa9333f6711cb7d132f887839eaab9be81f
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-18 21:29:26 +0800

    fix: 데이터 저장 위치 변경

--- src/component/Auth/KakaoLoginCallback.js
+++ src/component/Auth/KakaoLoginCallback.js
@@ -1,6 +1,9 @@
+import { async } from '@firebase/util';
 import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import PulseLoader from 'react-spinners/PulseLoader';
 import client from '../../api';
+import DataPush from '../Elements/DataPush';
 
 const override = {
   display: 'block',
@@ -8,29 +11,49 @@ const override = {
 };
 
 const KakaoLoginCallback = (props) => {
+  const dispatch = useDispatch();
   let [color, setColor] = useState('#EB8888');
+  let [IsLogin, setIsLogin] = useState(false);
+  const [IsMatching, setIsMatching] = useState(false);
 
   // 카카오 인가코드 추출
   let code = new URL(window.location.href).searchParams.get('code');
 
   // 서버에 인가코드 넘겨주기
   useEffect(() => {
-    client
-      .get(`/api/auth/kakao/callback?code=${code}`)
-      .then((res) => {
-        // 반환된 자체 토큰, 유저 정보 저장
-        // ###### 여기서 Access Token과 Refresh Token을 저장해주시면 됩니다! ######
-        console.log('로그인 성공');
-        window.sessionStorage.setItem('access', res.data.data.user.accessToken);
-        window.sessionStorage.setItem('refresh', res.data.data.user.refreshToken);
-        window.sessionStorage.setItem('id', res.data.data.user.id);
-        window.location.replace('/apply/14');
-      })
-      .catch((err) => {
-        console.log(err);
-        window.alert('로그인 실패');
-      });
-  }, []);
+    // 로그인 전
+    if (!IsLogin) {
+      client
+        .get(`/api/auth/kakao/callback?code=${code}`)
+        .then((res) => {
+          // 반환된 Access Token, Refresh Token, 유저 정보 저장
+          console.log('로그인 성공');
+          window.sessionStorage.setItem('access', res.data.data.user.accessToken);
+          window.sessionStorage.setItem('refresh', res.data.data.user.refreshToken);
+          window.sessionStorage.setItem('id', res.data.data.user.id);
+          setIsLogin(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          window.alert('로그인 실패');
+        });
+    }
+    // 로그인 후
+    else {
+      async function callDataPush() {
+        await DataPush();
+      }
+      callDataPush() // 매칭 정보 서버에 저장
+        .then(() => {
+          // setIsMatching(true);
+          // if (IsMatching) dispatch({ type: 'SET_IS_MATCHING', payload: props.IsMatching });
+          window.location.replace('/apply/15');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [IsLogin]);
 
   return (
     <div className="sweet-loading">
