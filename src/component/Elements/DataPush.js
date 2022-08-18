acf91582ee2cf3c5992b6e0f526e39b24431a9cc
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-08-18 19:17:32 +0800

    add: User - isMatching

--- src/component/Elements/DataPush.js
+++ src/component/Elements/DataPush.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import client from '../../api';
+
 function DataPush() {
+  const dispatch = useDispatch();
   const ourTeamInfo = JSON.parse(window.sessionStorage.getItem('ourTeam'));
   const prefferedTeamInfo = JSON.parse(window.sessionStorage.getItem('prefferedTeam'));
-
   const phonenumber = window.sessionStorage.getItem('phone');
   const id = window.sessionStorage.getItem('id');
   const finalOurTeamInfo = { ...ourTeamInfo, userId: parseInt(id) };
-  console.log(phonenumber);
   const postPhonenunber = { userId: parseInt(id), phone: phonenumber };
-  console.log('post', postPhonenunber);
+  const [IsMatching, setIsMatching] = useState(false);
+
   client
     .post('api/auth/phone', postPhonenunber, {
       headers: { authorization: `Bearer ${window.sessionStorage.getItem('access')}` },
@@ -30,6 +33,8 @@ function DataPush() {
               headers: { authorization: `Bearer ${window.sessionStorage.getItem('access')}` },
             })
             .then((res) => {
+              setIsMatching(true);
+              if (IsMatching) dispatch({ type: 'SET_IS_MATCHING', payload: props.IsMatching });
               alert('완성');
             })
             .catch((err) => {
@@ -42,6 +47,4 @@ function DataPush() {
     })
     .catch((err) => {});
 }
-export default DataPush();
-/*
- */
+export default DataPush;
