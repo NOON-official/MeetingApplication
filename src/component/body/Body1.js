805fc0209d8056ea27d81144b8a0cf36db1d98c0
yeonseong <yeonseong.dev@gmail.com>
2022-09-17 00:29:57 +0900

    FEAT: 문자로 보내줄 join링크 추가

--- src/component/body/Body1.js
+++ src/component/body/Body1.js
@@ -6,20 +6,33 @@ import MatchingInquire from '../mainpages/MatchingInquire';
 import { useSelector } from 'react-redux';
 import { Container } from '../Elements/StyledComponent';
 import MyPage from '../mainpages/MyPage';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+
 const Body1 = () => {
+  const dispatch = useDispatch();
+  const pageChange = async(state)=>{
+    return(dispatch({type: 'PAGE', payload: state}));
+  }
+  useEffect(async () => {
+    const location = window.location.pathname;
+    if (location === '/join')
+      pageChange(2);
+  }, [])
+
   const pagestate = useSelector((state)=> state.pagestate);
-  
+
   const Body = useCallback(()=>{
-    if (pagestate==0)
-  {return(<Main/>)}
-  else if( pagestate ==1)
-  return(<Guide/>)
-  else if (pagestate==2)
-  return(<MatchingInquire/>)
-  else if(pagestate==3)
-  return(<MyPage/>)
-},
-  [pagestate]) ;
+  if (pagestate == 0)
+    return(<Main/>)
+  else if(pagestate == 1)
+    return(<Guide/>)
+  else if (pagestate == 2)
+    return(<MatchingInquire/>)
+  else if(pagestate == 3)
+    return(<MyPage/>)
+}, [pagestate]) ;
   
 
   return (
