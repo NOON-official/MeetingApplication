0381edf3248ddd08ac707ec05fa9ea2df754a515
kodh0206 <david0206@naver.com>
2022-09-30 13:22:24 +0900

    테이블에 데이터 표시 구현완료

--- src/component/TestPage.js
+++ src/component/TestPage.js
@@ -11,10 +11,10 @@ const FullDiv = styled.div`
 
 const TestPage =  () => {
 
-  const [maleThreeTeam , setMaleThreeTeam] = useState();
-  const [maleTwoTeam, setMaleTwoTeam] =useState();
-  const [femaleTwoTeam, setFemaleTwoTeam] =useState();
-  const [femaleThreeeTeam, setFemaleThreeTeam] =useState();
+  const [maleThreeTeam , setMaleThreeTeam] = useState([]);
+  const [maleTwoTeam, setMaleTwoTeam] =useState([]);
+  const [femaleTwoTeam, setFemaleTwoTeam] =useState([]);
+  const [femaleThreeeTeam, setFemaleThreeTeam] =useState([]);
   function hideColumn() {
     const column = document.getElementById('others');
     column.style.display = 'none';
@@ -30,9 +30,16 @@ const TestPage =  () => {
   const columns1=['id','여성유저 이름','인원','나이','희망나이','희망요일']//여성유저(2명) 테이블 배열
   const columns2=['id','남성유저 이름','인원','나이','희망나이','희망요일']//남성유저(2명) 테이블 배열
   const columns3=['id','여성유저 이름','인원','나이','희망나이','희망요일']//여성유저(3명) 테이블 배열
-  const columns4=['id','남성유저 이름','인원','나이','희망나이','희망요일']//남성유저(3명) 테이블 배열
+  const columns4=['id','남성유저 이름','인원','나이','희망나이','희망 요일']//남성유저(3명) 테이블 배열
   
-  let data=[0,0,0];
+  const data = [
+    ['me', 'a', 'info'],
+    ['me', 'b', 'info'],
+    ['me', 'c', 'info'],
+    ['he', 'c', 'info'],
+    ['he', 'd', 'info'],
+  ];
+
   let data1 =[];//남성유저 3명 데이터
   let data2 =[];//남성유저 2명 데이터
   let data3 =[];//여성유저 3명 데이터
@@ -44,113 +51,50 @@ useEffect(()=>{
   
      setMaleThreeTeam(res.data.data.maleTeam)
      .then(()=>{
-      console.log("1",maleThreeTeam)
+      console.log("1",maleThreeTeam);
       })
    
         //console.log(maleThreeTeam[0]['userId'])
     })
     .catch((err)=>{})
-
-
-
 },[])
-console.log("2",maleThreeTeam)
-/*
+
+console.log("2",maleThreeTeam);
+console.log("0번째",maleThreeTeam[0]);
+
 for(let i=0; i<maleThreeTeam.length;i++)
 {
-  data1[i] ={'age' : maleThreeTeam[i]['age'], 'preferenceAge' : maleThreeTeam[i]['preferenceAge']}
+  data1[i] ={'userID' : maleThreeTeam[i]['userId'],'name': "민수",'num': maleThreeTeam[i]['num'],'age' : maleThreeTeam[i]['age'], 'preferenceAge' : maleThreeTeam[i]['preferenceAge'],'day':maleThreeTeam[i]['day']}
 }
 console.log(data1)
-*/
-  let sameuser = [];
-  let nowUserNum = 0;
-  let nowUserName = data[0][0];
-  data.map((user, idx) => {
-    if (user[0] === nowUserName) {
-      nowUserNum += 1;
-    } else {
-      sameuser.push([nowUserName, nowUserNum]);
-      nowUserNum = 1;
-      nowUserName = user[0];
-    }
-    if (idx === data.length - 1) {
-      sameuser.push([user[0], nowUserNum]);
-    }
-  });
-  let prevUserName = null;
+
   return (
     <div>
-      <div> 매칭 리스트업</div>
-      <StyledDiv left="50%" width="100%">
-        <table width="100%" border="1" bordercolor="blue">
-          <tr>
-            <td>
-              <td id="womanName"> 여성유저 이름</td>
-              <td id="womanNum">인원</td>
-              <td id="womanAge">나이</td>
-              <td id="womanPrefferedAge">희망나이</td>
-              <td id="womanDay">요일</td>
-            </td>
-            <td>
-              <td id="manName"> 남성유저 이름</td>
-              <td id="manNum">인원</td>
-              <td id="manAge">나이</td>
-              <td id="manPrefferedAge">희망나이</td>
-              <td id="manDay">요일</td>
-              <td id="others">기타</td>
-              <td>매칭버튼</td>
-            </td>
-          </tr>
-          {data.map((data, idx) => {
-            if (data[0] !== prevUserName) {
-              return sameuser.map((sameuserdata) => {
-                if (sameuserdata[0] === data[0]) {
-                  console.log('true');
+      <div style={{
+        marginBottom :'30px'
 
-                  prevUserName = data[0];
-                  return (
-                    <tr key={idx}>
-                      <td rowSpan={sameuserdata[1]}>
-                        <td id="womanName"> {data[0]}</td>
-                        <td id="womanNum">인원</td>
-                        <td id="womanAge">나이</td>
-                        <td id="womanPrefferedAge">희망나이</td>
-                        <td id="womanDay">요일</td>
-                      </td>
-                      <td>
-                        <td id="manName"> {data[1]}</td>
-                        <td id="manNum">인원</td>
-                        <td id="manAge">나이</td>
-                        <td id="manPrefferedAge">희망나이</td>
-                        <td id="manDay">요일</td>
-                        <td id="others">{data[2]}</td>
-                        <td>
-                          <button />
-                        </td>
-                      </td>
-                    </tr>
-                  );
-                }
-              });
-            } else {
-              return (
-                <tr key={idx}>
-                  <td>
-                    <td id="manName"> {data[1]}</td>
-                    <td id="manNum">인원</td>
-                    <td id="manAge">나이</td>
-                    <td id="manPrefferedAge">희망나이</td>
-                    <td id="manDay">요일</td>
-                    <td id="others">{data[2]}</td>
-                    <td>
-                      <button />
-                    </td>
-                  </td>
-                </tr>
-              );
-            }
-          })}
-        </table>
+      }}> 매칭 리스트업</div>
+      <StyledDiv left="30%" width="50%" height="200px">
+      <table border="1"color="blue">
+        <thead >
+          {columns2.map((column)=>
+          (<th style={{width:"5vw"}}key={column}>{column}</th>)
+          )}
+        </thead>
+       
+        <tbody>
+        {data1.map(({ userID, name, num,age,preferenceAge,day }) => (
+          <tr key={userID + name+ num+age+preferenceAge+day}>
+            <td>{userID}</td>
+            <td>{name}</td>
+            <td>{num}</td>
+            <td>{age}</td>
+            <td>{preferenceAge}</td>
+            <td>{day}</td>
+          </tr>
+        ))}
+      </tbody>
+      </table>
         <button
           onClick={() => {
             hideColumn();
