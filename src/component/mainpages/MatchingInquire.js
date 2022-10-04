0971e2cb81e7ebb765d0db2799c7162dad121e01
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2022-10-04 23:21:32 +0900

    FIX: 수정

--- src/component/mainpages/MatchingInquire.js
+++ src/component/mainpages/MatchingInquire.js
@@ -12,11 +12,13 @@ import { useSelector } from 'react-redux';
 const MatchingInquire = () => {
   const [userState, setUserState] = useState (window.localStorage.getItem('matchingStatus')?window.localStorage.getItem('matchingStatus'):null);
   let status
-  const isLogin = useSelector((state)=> state.userLogin)
+  let isLogin = useSelector((state)=> state.userLogin)
   let ourteamId =window.localStorage.getItem('ourteamId')&&window.localStorage.getItem('ourteamId')
   useEffect( ()=>{
 
     if (isLogin){
+      if(ourteamId!= -1){
+      console.log("start");
       client
       .get(`api/team/status/${ourteamId}`)
       .then((res)=>{
@@ -30,7 +32,7 @@ const MatchingInquire = () => {
         window.localStorage.setItem('matchingStatus',status);
       })
       .catch((err)=>{})
-  
+    }
     }
     
   },[])
