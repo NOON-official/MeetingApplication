01efb6bc1c3a07693b9e5e0aed6ae112ff16a3ac
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-04 11:46:54 +0900

    ADD:folder

---
+++ frontend/src/Style/globalStyle.js
@@ -0,0 +1,9 @@
+export const globalStyle = {
+  font: "Pretendard",
+  grey: "#80808029",
+  pink: "#eb8888",
+  lightPink: "#B79292",
+  smallText: "10px",
+  mediumText: "20px",
+  largeText: "30px",
+};
