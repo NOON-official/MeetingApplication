b00618979f9302df378b17c1197650b30e05b175
Dongkeun <hongdongk@naver.com>
2023-01-05 10:21:13 +0900

    fix: theme

--- frontend/src/Style/theme.js
+++ frontend/src/Style/theme.js
@@ -1,7 +1,9 @@
 export const theme = {
   font: "Pretendard",
+  width: `${window.innerWidth / 200}`,
   grey: "#80808029",
   pink: "#eb8888",
+  background: "#F8F3F3",
   lightPink: "#ffb9b9",
   smallText: "10px",
   mediumText: "20px",
--- frontend/src/layout/header/TopHeader.js
+++ frontend/src/layout/header/TopHeader.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Header } from "../../asset/svg/Header.svg";
+import { theme } from "../../Style/theme";
 
 const TopHeader = () => {
   return (
@@ -17,11 +18,13 @@ const TopHeader = () => {
 export default TopHeader;
 
 const Container = styled.div`
-  padding: 10px 20px;
+  width: 100%;
   background-color: ${(props) => props.theme.lightpink};
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const LoginBox = styled.div``;
+const LoginBox = styled.div`
+  padding-right: ${theme.width * 2}px;
+`;
