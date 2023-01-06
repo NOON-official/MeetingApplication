ae1779d0cbf43518025afe88db0cbe41c75579c3
Dongkeun <hongdongk@naver.com>
2023-01-06 14:55:37 +0900

    FIX: reducer

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router";
+import { Provider } from "react-redux";
 
 import { theme } from "./Style/theme";
 import Router from "./Router";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={Router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={Router} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/reducers/user.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducer: {},
+});
+
+export default userSlice;
