89ac6ec7a85482ac503e2be9658b929c08940e09
Dongkeun <hongdongk@naver.com>
2023-01-06 14:22:51 +0900

    ADD: folder(redux)

--- src/components/guideBox.js
+++ src/components/guideBox.js
@@ -1,18 +1,22 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
+
+import styled from "styled-components";
 import { ReactComponent as DownArrow } from "../asset/svg/DownArrow.svg";
 import { ReactComponent as UpArrow } from "../asset/svg/UpArrow.svg";
-import styled from "styled-components";
+
 const GuideBox = (props) => {
   const [commentOpened, setCommentOpened] = useState(false);
   const onToggleComment = useCallback(() => {
     setCommentOpened((prev) => !prev);
   }, []);
+
   return (
     <SubTitle key={props.title}>
       {props.title}
       <ArrowBox onClick={onToggleComment}>
         {commentOpened ? <UpArrow /> : <DownArrow />}
         {commentOpened && <div>{props.content}</div>}
+        {commentOpened && props.link && <a href={props.link}>자세히보기</a>}
       </ArrowBox>
     </SubTitle>
   );
--- src/pages/main/guide.js
+++ src/pages/main/guide.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import GuideBox from "../../components/guideBox";
