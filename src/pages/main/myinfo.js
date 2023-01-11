d5abd620a6a9440a2dead1d7a9f1b79277f450fb
Dongkeun <hongdongk@naver.com>
2023-01-11 17:49:52 +0900

    FIX: myinfo

--- src/pages/main/myinfo.js
+++ src/pages/main/myinfo.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import { theme } from "../../Style/theme";
 import MainFooter from "../../layout/footer/mainFooter";
 import MainLayOut from "../../layout/mainlayout";
+// import { ReactComponent as Invitefreinds } from "../../asset/svg/Invitefreinds.svg";
 
 const MyInfo = () => {
   return (
@@ -12,9 +14,15 @@ const MyInfo = () => {
         <Item><SLink to="">이용권현황</SLink></Item>
         <LItem><SLink to="">제안하기</SLink></LItem>
       </Account>
-      <Account>
-        
-      </Account>
+      <CouponBox>
+        <Title>친구 4명을 초대하면 스타벅스 커피 1잔 쿠폰을 드려요!</Title>
+        <Coupons>
+          <Circle>1</Circle>
+          <Circle>2</Circle>
+          <Circle>3</Circle>
+          <Circle>4</Circle>
+        </Coupons>
+      </CouponBox>
       <MainFooter/>
     </MainLayOut>
   );
@@ -24,16 +32,20 @@ const MyInfo = () => {
 export default MyInfo;
 
 const Account = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
   background-color: white;
   border: 1px solid #F8F3F3;
   border-radius: 10px;
-  padding: 10px 20px;
   margin-top:21px;
   width: 334px;
   height: 158px;
 `;
 
 const Item = styled.div`
+  width:85%;
+  padding-left:10px;
   font-weight: 600;
   font-size: 14px;
   color: #777777;
@@ -43,6 +55,8 @@ const Item = styled.div`
 `;
 
 const LItem = styled.div`
+  width:85%;
+  padding-left:10px;
   height:50px;
   line-height:50px;
 `;
@@ -52,4 +66,48 @@ const SLink = styled(Link)`
   font-size: 14px;
   color: #777777;
 `;
+
+const CouponBox = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  background-color: white;
+  border: 1px solid #F8F3F3;
+  border-radius: 10px;
+  width: 334px;
+  height: 158px;
+`;
+
+const Title = styled.p`
+  margin-top:10px;
+  height:10px;
+  text-align:center;
+  width:100%;
+  font-weight: 200;
+  font-size: 12px;
+  color: #000000;
+`;
+
+const Coupons = styled.div`
+  margin-bottom:25px;
+  width:90%;
+  display: flex;
+  justify-content:center;
+  justify-content:space-between;
+`;
+
+const Circle = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  line-height:65px;
+  width: 65px;
+  height: 65px;
+  color: #777777;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius:50%;
+  background-color: ${(props) => `${props.isactive ? `${theme.lightPink}` : "#ECE9E9"}`};
+`;
+
 
