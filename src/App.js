cc5d0a17e361fffab832f56af4fcd36f2412bdaa
Darron Park <darron1217@gmail.com>
2023-01-12 13:41:48 +0900

    chore: redux, 디렉토리 구조 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import GlobalStyle from "./Style/global";
+import GlobalStyle from "./style/global";
 
 function App() {
   return (
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import user from "./features/user";
+import applySlice from "./features/apply";
+import matchingSlice from "./features/matching";
+
+const createStore = () => {
+  const store = configureStore({
+    reducer: {
+      user: user,
+      apply: applySlice.reducer,
+      matching: matchingSlice.reducer,
+    },
+  });
+  return store;
+};
+
+export default createStore;
