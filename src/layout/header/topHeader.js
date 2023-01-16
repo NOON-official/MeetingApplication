6c33b8c5bfd847b070faf78c289dbcd843977bb9
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-16 10:56:28 +0900

    commit

--- src/layout/header/topHeader.js
+++ src/layout/header/topHeader.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
 import { ReactComponent as Header } from "../../asset/svg/Header.svg";
 import { theme } from "../../style/theme";
 import { useCallback, useState } from "react";
-import KakaoLoigin from '../../util/login/kakaoLogin';
+import KakaoLoigin from "../../util/login/kakaoLogin";
 
 const TopHeader = () => {
   const [login, setLogin] = useState(false);
