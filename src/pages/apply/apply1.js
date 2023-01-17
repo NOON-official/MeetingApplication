593a440142c057f022bff1f5c7a48a8aa1f941d9
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-17 11:56:32 +0900

    NEW: Gyuha

--- src/pages/apply/apply1.js
+++ src/pages/apply/apply1.js
@@ -1,19 +1,22 @@
-import TopHeader from "../../layout/header/topHeader";
-import styled from "styled-components";
-import { Container } from "../../components/Container/BasicContainer";
-import { theme } from "../../style/theme";
-import { TextColorBinary } from "../../util/TextColorBinary";
-import { useCallback, useMemo, useState } from "react";
-import { BinaryButton } from "../../components/button";
+import { useCallback, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import TopHeader from '../../layout/header/topHeader';
+import BasicContainer from '../../components/Container/BasicContainer';
+import theme from '../../style/theme';
+import TextColorBinary from '../../util/TextColorBinary';
+import BinaryButton from '../../components/button';
+import Universities from '../../asset/Universities';
+import IsPageCompleteModal from '../../components/Modal/IsPageCompleteModal';
 
-const Apply1 = () => {
-  const title = "우리 팀을 소개해주세요";
-  const title2 = "우리 팀의 학교는?";
+export default function Apply1() {
+  const title = '우리 팀을 소개해주세요';
+  const title2 = '우리 팀의 학교는?';
   const [openModal, setOpenModal] = useState(false);
   const [selectedUniversities, setSelectedUniversities] = useState([1]);
+
   const [gender, setGender] = useState(true);
   const [number, setNumber] = useState(true);
-  const [searchKeyWord, setSearchKeyWord] = useState("0");
+  const [searchKeyWord, setSearchKeyWord] = useState('0');
   const inputChange = (e) => {
     setSearchKeyWord(e.target.value);
   };
@@ -21,74 +24,46 @@ const Apply1 = () => {
     selectedUniversities.push(university);
   }
   const SearchedUniversities = useCallback(() => {
-    let data;
-    data = Universities.filter((c) => {
-      return c["univ"].indexOf(searchKeyWord) > -1;
+    const data = Universities.filter((c) => {
+      return c.univ.indexOf(searchKeyWord) > -1;
     });
     return data.map((c, index) => (
       <SearchedUniversity
         onClick={() => {
           OnUniversityClick(c);
         }}
+        // eslint-disable-next-line react/no-array-index-key
         key={index}
       >
-        {c["univ"]}
+        {c.univ}
       </SearchedUniversity>
     ));
   }, [searchKeyWord, selectedUniversities]);
-  const SelectedNumUniversity = (props) => {
-    const width = useMemo(() => {
-      if (selectedUniversities.length == 1) {
-        return "45%";
-      } else if (selectedUniversities.length == 2) {
-        return "45%";
-      } else if (selectedUniversities.length == 3) {
-        return "30%";
-      } else if (selectedUniversities.length == 4) {
-        return "23%";
-      }
-    }, [selectedUniversities]);
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: { width },
-          color: "#FFFFFF",
-          backgroundColor: "#EB8888",
-          alignItems: "center",
-          textAlign: "center",
-          minHeight: "40px",
-        }}
-      >
-        <div style={{ font: "Nanum JungHagSaeng" }}>{props.university}</div>
-      </div>
-    );
-  };
+
   function binarySearch(arr, target) {
     let start = 0;
     let end = arr.length - 1;
     let mid;
 
     while (start <= end) {
-      //점점 좁혀지다가 start와 end의 순서가 어긋나게 되면 반복을 종료한다
+      // 점점 좁혀지다가 start와 end의 순서가 어긋나게 되면 반복을 종료한다
 
       mid = parseInt((start + end) / 2);
 
-      if (target === arr[mid]["key"]) {
-        return arr[mid]["univ"];
+      if (target === arr[mid]['key']) {
+        return arr[mid]['univ'];
+      }
+      if (target < arr[mid]['key']) {
+        end = mid - 1;
       } else {
-        if (target < arr[mid]["key"]) {
-          end = mid - 1;
-        } else {
-          start = mid + 1;
-        }
+        start = mid + 1;
       }
     }
     return -1;
   }
   return (
-    <Container>
+    <BasicContainer>
+      <IsPageCompleteModal open={openModal} />
       <TopHeader />
       <ScrollDiv>
         <TextColorBinary
@@ -113,23 +88,122 @@ const Apply1 = () => {
             condition1="2:2"
             condition2="3:3"
             onChange={(result) => setNumber(result)}
-          ></BinaryButton>
-          <TextColorBinary
-            text={title2}
-            colorFirst={theme.pink}
-            colorSecond={theme.black}
-            num={8}
           />
-          <SubTitle>팀원들의 모든 학교를 말해주세요</SubTitle>
-        </ButtonDiv>
+        </ButtonDiv>{' '}
+        <TextColorBinary
+          text={title2}
+          colorFirst={theme.pink}
+          colorSecond={theme.black}
+          num={8}
+        />
+        <SubTitle>팀원들의 모든 학교를 말해주세요</SubTitle>
+        <UniversityDiv>
+          {selectedUniversities.length === 0 ? (
+            console.log
+          ) : (
+            <SelectedDiv>
+              {Universities.map((data, idx) => {
+                const univ = binarySearch(Universities, data);
+                return null;
+                // return (
+                //   <SelectedNumUniversity
+                //     key={idx}
+                //     university={univ}
+                //     selectedUniversities={selectedUniversities}
+                //   />
+                // );
+              })}
+            </SelectedDiv>
+          )}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              width: '100%',
+              height: '44px',
+            }}
+          >
+            <input
+              onChange={inputChange}
+              name="universitySearch"
+              placeholder="학교를 검색해주세요"
+            />
+          </div>
+          <div
+            style={{
+              overflowX: 'hidden',
+              overflow: 'scroll',
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              height: '200px',
+            }}
+          >
+            <SearchedUniversities />
+          </div>
+        </UniversityDiv>
+        <button
+          type="button"
+          onClick={() => {
+            console.log(openModal);
+            setOpenModal(true);
+          }}
+        />
       </ScrollDiv>
     </BasicContainer>
   );
-};
-
+}
+function SelectedNumUniversity(props) {
+  const { selectedUniversities } = props;
+  const width = useMemo(() => {
+    if (selectedUniversities.length === 1) {
+      return '45%';
+    }
+    if (selectedUniversities.length === 2) {
+      return '45%';
+    }
+    if (selectedUniversities.length === 3) {
+      return '30%';
+    }
+    if (selectedUniversities.length === 4) {
+      return '23%';
+    }
+    return '45%';
+  }, [selectedUniversities]);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: { width },
+        color: '#FFFFFF',
+        backgroundColor: '#EB8888',
+        alignItems: 'center',
+        textAlign: 'center',
+        minHeight: '40px',
+      }}
+    >
+      <div style={{ font: 'Nanum JungHagSaeng' }}>{props.university}</div>
+    </div>
+  );
+}
+const SelectedDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: '0 0 0 10px';
+  align-items: center;
+  width: 100%;
+  height: 50px;
+`;
+const UniversityDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 60%;
+`;
 const SubTitle = styled.text`
-  color: "#AAAAAA";
-
+  display: flex;
+  color: '#AAAAAA';
   font-size: 13px;
   font-weight: 500;
 `;
@@ -141,7 +215,6 @@ const TitleOfButton = styled.text`
 `;
 const ButtonDiv = styled.div`
   height: 77px;
-
   flex: 1 0 auto;
   display: flex;
   flex-direction: column;
@@ -169,4 +242,3 @@ const SearchedUniversity = styled.div`
   border-bottom: 1px solid #f6eeee;
   overflow-x: hidden;
 `;
-export default Apply1;
