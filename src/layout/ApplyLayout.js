b8f252c0a7cfb3a12ef516088781241a61aef233
Dongkeun <hongdongk@naver.com>
2023-01-20 11:53:08 +0900

    apply2스타일수정

--- src/layout/ApplyLayout.js
+++ src/layout/ApplyLayout.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-import theme from '../style/theme';
 import TopHeader from './header/TopHeader';
 
 function ApplyLayout({ children }) {
   return (
     <Container>
-      <TopHeader />
+      <Header>
+        <TopHeader />
+      </Header>
       <Content>{children}</Content>
     </Container>
   );
@@ -15,16 +16,18 @@ export default ApplyLayout;
 
 const Container = styled.div`
   min-height: 100vh;
-  background-color: ${theme.background};
   display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Header = styled.header`
+  width: 100%;
 `;
 
 const Content = styled.div`
   width: 400px;
   min-height: calc(100vh - 70px);
-  background-color: #fbfaf9;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
