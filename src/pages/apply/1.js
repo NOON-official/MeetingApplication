090d4131c6f080823dc21b19a39a1518a36df869
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-20 10:58:12 +0900

    apply1 -> 대학 선택시 취소 구현안됨

--- src/pages/apply/1.js
+++ src/pages/apply/1.js
@@ -7,9 +7,11 @@ import TextColorBinary from '../../util/TextColorBinary';
 import BinaryButton from '../../components/BinaryButton';
 import Universities from '../../asset/Universities';
 import IsPageCompleteModal from '../../components/Modal/IsPageCompleteModal';
+import { ReactComponent as Xbutton } from '../../asset/svg/Xbutton.svg';
+import { ReactComponent as SearchIcon } from '../../asset/svg/SearchIcon.svg';
 
 export default function Apply1() {
-  const title = '우리 팀을 소개해주세요';
+  const title = '기본 정보를 알려주세요';
   const title2 = '우리 팀의 학교는?';
   const [openModal, setOpenModal] = useState(false);
   const [selectedUniversities, setSelectedUniversities] = useState([]);
@@ -77,10 +79,12 @@ export default function Apply1() {
           text={title}
           colorFirst={theme.pink}
           colorSecond={theme.black}
-          num={8}
+          num={5}
         />
+        <SizedBox />
         <ButtonDiv>
           <TitleOfButton>성별</TitleOfButton>
+
           <BinaryButton
             state={gender}
             condition1="남자"
@@ -96,14 +100,17 @@ export default function Apply1() {
             condition2="3:3"
             onChange={(result) => setNumber(result)}
           />
-        </ButtonDiv>{' '}
+        </ButtonDiv>
+        <SizedBox height="50px" />
         <TextColorBinary
           text={title2}
           colorFirst={theme.pink}
           colorSecond={theme.black}
           num={8}
         />
+        <SizedBox />
         <SubTitle>팀원들의 모든 학교를 말해주세요</SubTitle>
+        <SizedBox height="20px" />
         <UniversityDiv>
           {selectedUniversities.length === 0 ? null : (
             <SelectedDiv>
@@ -121,20 +128,38 @@ export default function Apply1() {
               })}
             </SelectedDiv>
           )}
+          <SizedBox height="20px" />
           <div
             style={{
               display: 'flex',
               justifyContent: 'space-between',
               flexDirection: 'row',
-              width: '100%',
+              width: '90%',
+              borderRadius: '10px',
+              borderStyle: 'solid',
+              borderWidth: '1px',
+              borderColor: '#EB8888',
+
               height: '44px',
             }}
           >
             <input
               onChange={inputChange}
               name="universitySearch"
+              style={{
+                display: 'flex',
+                width: '100%',
+                height: '100%',
+                fontSize: '18px',
+                border: '0',
+                textAlign: 'center',
+                outline: 'none',
+                marginLeft: '10px',
+                backgroundColor: 'transparent',
+              }}
               placeholder="학교를 검색해주세요"
             />
+            <SearchIcon />
           </div>
           <div
             style={{
@@ -144,6 +169,7 @@ export default function Apply1() {
               alignItems: 'center',
               flexDirection: 'column',
               height: '200px',
+              width: '90%',
             }}
           >
             <SearchedUniversities />
@@ -172,7 +198,7 @@ function SelectedNumUniversity(props) {
       return '30%';
     }
     if (selectedUniversities.length === 4) {
-      return '23%';
+      return '22%';
     }
     return '45%';
   }, [selectedUniversities]);
@@ -181,23 +207,35 @@ function SelectedNumUniversity(props) {
       style={{
         display: 'flex',
         justifyContent: 'space-between',
-        width: { width },
+        width: `${width}`,
         color: '#FFFFFF',
         backgroundColor: '#EB8888',
         alignItems: 'center',
         textAlign: 'center',
         minHeight: '40px',
+        borderRadius: '10px',
+        padding: ' 5px 5px 5px 5px',
       }}
       onClick={() => {}}
     >
-      <div style={{ font: 'Nanum JungHagSaeng' }}>{props.university}</div>
+      <UniversityText style={{ size: '15px' }}>
+        {props.university}
+      </UniversityText>
+      <Xbutton />
     </div>
   );
 }
+
+const UniversityText = styled.text`
+  font-size: 10px;
+`;
+const SizedBox = styled.div`
+  height: ${(props) => props.height || '10px'};
+`;
 const SelectedDiv = styled.div`
   display: flex;
   justify-content: space-evenly;
-  margin: '0 0 0 10px';
+  margin: '0 0 0 5%';
   align-items: center;
   width: 100%;
   height: 50px;
@@ -206,6 +244,7 @@ const UniversityDiv = styled.div`
   display: flex;
   flex-direction: column;
   height: 60%;
+  width: 100%;
 `;
 const SubTitle = styled.text`
   display: flex;
@@ -214,23 +253,26 @@ const SubTitle = styled.text`
   font-weight: 500;
 `;
 const TitleOfButton = styled.text`
+  margin: 10px 10px 10px 10px;
   color: #777777;
   font-size: 14px;
   font-family: ${theme.Prefont};
   font-weight: 500;
 `;
 const ButtonDiv = styled.div`
+  width: 100%;
+  max-width: 375px;
   height: 77px;
   flex: 1 0 auto;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
 `;
 
 const ScrollDiv = styled.div`
-  padding: 20px 10px 20px 10px;
   max-width: 400px;
   flex: 1 0 auto;
-  width: 100%;
+  width: 90%;
   align-items: flex-start;
   min-height: calc(100vh - 120px);
 `;
