7c6cbfb83a159e6429d733b85db29b2ba9efe0bf
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-25 02:13:21 +0900

    calendar css

--- src/style/global.js
+++ src/style/global.js
@@ -49,6 +49,33 @@ table {
 	border-collapse: collapse;
 	border-spacing: 0;
 }
+.custom-calendar.rmdp-shadow {
+	box-shadow: none;
+}
+.custom-calendar.rmdp-wrapper{
+	border: 1px solid #F49393;
+	border-radius: 10px;
+}
+.rmdp-arrow {
+	border: solid black;
+    border-width: 0 2px 2px 0;
+    display: inline-block;
+    height: 3px;
+    margin-top: 5px;
+    padding: 2px;
+    width: 3px;
+}
+.rmdp-week-day {
+    color: black;
+    cursor: default;
+    font-size: 12px;
+    font-weight: 400;}
+	.rmdp-day.rmdp-selected span:not(.highlight) {
+    background-color: #F49393;
+    box-shadow: 0 0 3px #8798ad;
+    color: #fff;
+}
+
 `;
 
 export default GlobalStyle;
