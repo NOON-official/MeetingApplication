4da38e4774f8ebc7db5a3477b39c97624a332580
GyuhaWang <86870218+GyuhaWang@users.noreply.github.com>
2023-01-25 02:13:21 +0900

    calendar css

--- src/pages/apply/1.js
+++ src/pages/apply/1.js
@@ -53,27 +53,7 @@ export default function Apply1() {
       </SearchedUniversity>
     ));
   }, [searchKeyWord, selectedUniversities]);
-  // function binarySearch(arr, target) {
-  //   let start = 0;
-  //   let end = arr.length - 1;
-  //   let mid;
-
-  //   while (start <= end) {
-  //     // 점점 좁혀지다가 start와 end의 순서가 어긋나게 되면 반복을 종료한다
-
-  //     mid = parseInt((start + end) / 2);
-
-  //     if (target === arr[mid]['key']) {
-  //       return arr[mid]['univ'];
-  //     }
-  //     if (target < arr[mid]['key']) {
-  //       end = mid - 1;
-  //     } else {
-  //       start = mid + 1;
-  //     }
-  //   }
-  //   return -1;
-  // }
+  
   return (
     <BasicContainer>
       <IsPageCompleteModal open={openModal} setModal={setModal} />
