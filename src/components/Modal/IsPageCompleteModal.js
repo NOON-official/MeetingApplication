4cf067f54e9d42084c9aae0630433de8dc943401
Dongkeun <hongdongk@naver.com>
2023-01-25 21:37:28 +0900

    모달수정

--- src/components/Modal/IsPageCompleteModal.js
+++ src/components/Modal/IsPageCompleteModal.js
@@ -1,70 +1,59 @@
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import styled from 'styled-components';
 import { ReactComponent as ErrorChar } from '../../asset/svg/ErrorMessageCharacter.svg';
 import theme from '../../style/theme';
 
-const BlackText = styled.text`
+export default function IsPageCompleteModal({ open, setModal }) {
+  return (
+    <div>
+      {open ? (
+        <Modal
+          open={open}
+          footer={null}
+          centered
+          width="380px"
+          closable={false}
+        >
+          <Container>
+            <ErrorChar />
+            <TextBox>
+              <BlackText>아직</BlackText>
+              <ColorText>완료하지 않은 답변이</ColorText>
+              <BlackText>있어요.</BlackText>
+            </TextBox>
+            <div>모든 항목에 응답해 주세요.</div>
+            <SButton onClick={() => setModal(false)}>닫기</SButton>
+          </Container>
+        </Modal>
+      ) : null}
+    </div>
+  );
+}
+const Container = styled.div`
+  padding-top: 5%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+const TextBox = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+const BlackText = styled.span`
   color: black;
   font-size: 35px;
   font-family: 'Nanum JungHagSaeng';
 `;
-const ColorText = styled.text`
+const ColorText = styled.span`
   color: ${theme.pink};
   font-size: 35px;
   font-family: 'Nanum JungHagSaeng';
 `;
 
-const Padding = styled.div`
-  width: 90%;
-  padding: 30px 30px 30px 30px;
+const SButton = styled(Button)`
+  margin-top: 10%;
+  width: 100%;
+  height: 50px;
+  color: white;
+  background-color: ${theme.pink};
 `;
-export default function IsPageCompleteModal(props) {
-  const { open, colse, setModal } = props;
-
-  return (
-    <div className={open ? 'openMidal modal' : 'modal'}>
-      {open ? (
-        <div
-          style={{
-            transform: 'translate(-50%, -50%)',
-            position: 'fixed',
-
-            top: '50%',
-            left: '50%',
-
-            borderRadius: '10px',
-            height: '50%',
-            width: '90%',
-            backgroundColor: 'white',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <ErrorChar />
-          <text>
-            {' '}
-            <BlackText>아직</BlackText>
-            <ColorText>완료하지 않은 답변이</ColorText>
-            <BlackText>이 있어요.</BlackText>
-          </text>
-          <text>모든 항목에 응답해 주세요.</text>
-          <Padding>
-            <Button
-              style={{
-                width: '100%',
-                height: '50px',
-
-                backgroundColor: `${theme.pink}`,
-              }}
-              onClick={() => setModal(false)}
-            >
-              <tect style={{ color: 'white' }}>닫기</tect>
-            </Button>
-          </Padding>
-        </div>
-      ) : null}
-    </div>
-  );
-}
