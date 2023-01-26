5278416d65cddbfd36db71257225d24fa84445c3
Darron Park <darron1217@gmail.com>
2023-01-26 12:25:22 +0900

    feat: 내정보 메인 디자인 반영

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import AntdCustomization from './style/AntdCustomization';
 import GlobalStyle from './style/global';
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <AntdCustomization />
       <Outlet />
     </>
   );
