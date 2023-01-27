d528dcb9f3ca42262d713514341f27294a73a173
Darron Park <darron1217@gmail.com>
2023-01-27 15:32:39 +0900

    style: 가이드 화면 스타일 정리

--- src/components/GuideBox.js
+++ src/components/GuideBox.js
@@ -1,34 +1,71 @@
+import { Button } from 'antd';
 import React, { useState, useCallback } from 'react';
-
 import styled from 'styled-components';
 import { ReactComponent as DownArrow } from '../asset/svg/DownArrow.svg';
 import { ReactComponent as UpArrow } from '../asset/svg/UpArrow.svg';
 
-const SubTitle = styled.div`
-  margin-top: 10px;
-  background-color: white;
-`;
+export default function GuideBox(props) {
+  const [isShown, setIsShown] = useState(false);
+  const handleToggle = useCallback(() => {
+    setIsShown((prev) => !prev);
+  }, []);
 
-const ArrowBox = styled.div`
+  return (
+    <Accordion onClick={handleToggle}>
+      <TitleBox>
+        <Title>{props.title}</Title>
+        {isShown ? <UpArrow /> : <DownArrow />}
+      </TitleBox>
+      {isShown && (
+        <ContentBox>
+          {props.content}
+          {props.link && (
+            <div>
+              <MoreButton href={props.link}>자세히보기 →</MoreButton>
+            </div>
+          )}
+        </ContentBox>
+      )}
+    </Accordion>
+  );
+}
+
+const Accordion = styled.div`
+  background-color: #ffffff;
+  border: 1px solid #f1ecec;
+  border-radius: 10px;
+  padding: 0 22px;
   :hover {
     cursor: pointer;
   }
 `;
 
-export default function GuideBox(props) {
-  const [commentOpened, setCommentOpened] = useState(false);
-  const onToggleComment = useCallback(() => {
-    setCommentOpened((prev) => !prev);
-  }, []);
+const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0;
+`;
+const Title = styled.span`
+  font-size: 12px;
+`;
 
-  return (
-    <SubTitle key={props.title}>
-      {props.title}
-      <ArrowBox onClick={onToggleComment}>
-        {commentOpened ? <UpArrow /> : <DownArrow />}
-        {commentOpened && <div>{props.content}</div>}
-        {commentOpened && props.link && <a href={props.link}>자세히보기</a>}
-      </ArrowBox>
-    </SubTitle>
-  );
-}
+const ContentBox = styled.div`
+  border-top: 1px solid #f1ecec;
+  font-size: 11px;
+  line-height: 13px;
+  padding: 15px 0;
+
+  > p {
+    margin: 8px 0;
+  }
+`;
+
+const MoreButton = styled(Button)`
+  color: #eb8888;
+  border: 1px solid #eb8888;
+  border-radius: 15px;
+  margin: 8px 0;
+  font-size: 9px;
+  height: auto;
+`;
