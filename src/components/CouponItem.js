8a3d94830bd54868c86ceac7123bd1a92e5073e9
Darron Park <darron1217@gmail.com>
2023-02-06 11:24:34 +0900

    feat: 보유쿠폰 화면 구현

---
+++ src/components/CouponItem.js
@@ -0,0 +1,82 @@
+import { useMemo } from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Checkbox } from '../asset/svg/Checkbox.svg';
+import { ReactComponent as CheckboxChecked } from '../asset/svg/CheckboxChecked.svg';
+import { ReactComponent as CouponLogo } from '../asset/svg/CouponLogo.svg';
+
+export default function CouponItem({
+  onClick,
+  disabled,
+  checked,
+  title,
+  expireText,
+  tipText,
+}) {
+  const checkboxContent = useMemo(() => {
+    if (!onClick) {
+      return null;
+    }
+    return checked ? <CheckboxChecked /> : <Checkbox />;
+  });
+
+  return (
+    <Container onClick={onClick} className={disabled ? 'disabled' : ''}>
+      {checkboxContent}
+      <CouponBox>
+        <CouponLogo />
+        <CouponBoxContent>
+          <CouponTitle>{title}</CouponTitle>
+          <div>
+            <CouponExpireText>{expireText}</CouponExpireText>
+            <CouponTipText>{tipText}</CouponTipText>
+          </div>
+        </CouponBoxContent>
+      </CouponBox>
+    </Container>
+  );
+}
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  > svg {
+    padding: 15px;
+  }
+
+  &.disabled {
+    pointer-events: none;
+    filter: grayscale(1);
+  }
+`;
+
+export const CouponBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  border: 1px solid #eb8888;
+  border-radius: 3px;
+  padding: 10px 12px;
+`;
+
+export const CouponBoxContent = styled.div`
+  width: 100%;
+`;
+
+export const CouponTitle = styled.div`
+  font-weight: 500;
+  font-size: 13px;
+  color: #eb8888;
+  margin-bottom: 4px;
+`;
+
+export const CouponExpireText = styled.span`
+  font-size: 10px;
+  color: #777777;
+`;
+
+export const CouponTipText = styled.span`
+  font-weight: 400;
+  font-size: 6px;
+  color: #777777;
+  margin-left: 4px;
+`;
