f6e3167405c3a2e5b696acc6acc3acd3b479c92f
Darron Park <darron1217@gmail.com>
2023-02-08 14:37:20 +0900

    feat: jwt 갱신 로직 구현

--- src/features/user/asyncActions.js
+++ src/features/user/asyncActions.js
@@ -1,20 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import backend from '../../util/backend';
 
-export const getMyInfo = createAsyncThunk(
-  'user/getMyInfo',
-  async (data, { rejectWithValue }) => {
-    try {
-      const response = await backend.get('/users/my-info', data);
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.response.data);
-    }
-  },
-);
-
 export const logout = createAsyncThunk('user/logout', async () => {
   const response = await backend.get(`/auth/signout`);
 
   return response.data;
 });
+
+export const refreshJwtToken = createAsyncThunk(
+  'user/refreshJwtToken',
+  async () => {
+    const response = await backend.get(`/auth/refresh`, {
+      withCredentials: true,
+    });
+
+    return response.data;
+  },
+);
