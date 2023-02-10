473c63b4d65c707763c07ac7bd7959044d791ba7
Darron Park <darron1217@gmail.com>
2023-02-10 14:10:53 +0900

    feat: 토스 페이먼츠 연동

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,10 @@
 export const GENDER_MALE = 1;
 export const GENDER_FEMALE = 2;
+export const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 export const STORAGE_KEY_ACCESS_TOKEN = 'accessToken';
 export const STORAGE_KEY_REFRESH_TOKEN = 'refresh';
+export const STORAGE_KEY_ORDER_DATA = 'order-data';
 export const MEMBER_COUNT_LABELS = {
   2: '2:2',
   3: '3:3',
