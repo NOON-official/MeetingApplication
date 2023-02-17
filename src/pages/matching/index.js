75f8d9993615f42e91a4a2e6c903cc7b5f092e18
Dongkeun <hongdongk@naver.com>
2023-02-17 14:27:52 +0900

    add: 매칭상태페이지완료

--- src/pages/matching/index.js
+++ src/pages/matching/index.js
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useCallback, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import LoginMatchOk from '../../components/MatchingBox/LoginMatchOk';
@@ -9,75 +9,66 @@ import LoginMatchComplete from '../../components/MatchingBox/LoginMatchComplete'
 import LoginMatchFail from '../../components/MatchingBox/LoginMatchFail';
 import LoginOtherTeamRefused from '../../components/MatchingBox/LoginOtherTeamRefused';
 import LoginNoAnswer from '../../components/MatchingBox/LoginNoAnswer';
-import LoginMatchFailed from '../../components/MatchingBox/LoginMatchFailed';
 import LoginRefuse from '../../components/MatchingBox/LoginRefuse';
 import NoLogin from '../../components/MatchingBox/NoLogin';
 import MainFooter from '../../layout/footer/MainFooter';
 import MainLayout from '../../layout/MainLayout';
-import {
-  useGetUserTeamIdDataQuery,
-  useGetMatchingStatusQuery,
-} from '../../features/backendApi';
+import backend from '../../util/backend';
 
 function Matching() {
-  const { data: userTeamId } = useGetUserTeamIdDataQuery();
-  const { data: matchingStatus } = useGetMatchingStatusQuery();
+  const [myteamId, setMyteamId] = useState('');
+  const [matchingStatus, setMatchingStatus] = useState('');
   const { accessToken } = useSelector((state) => state.user);
 
-  const state = 3;
+  const getInformation = useCallback(async () => {
+    const teamid = await backend.get(`/users/team-id`);
+    const matchingstatus = await backend.get('/users/matchings/status');
+    setMyteamId(teamid.data.teamId);
+    setMatchingStatus(matchingstatus.data.matchingStatus);
+  }, []);
 
-  console.log(userTeamId?.teamId);
-  console.log(matchingStatus);
+  useEffect(() => {
+    getInformation();
+  }, []);
 
   const MatchingBox = useMemo(() => {
     if (!accessToken) {
-      return <NoLogin />; // 완료
+      return <NoLogin />; // 로그인안했을때!
     }
-    if (state === 1) {
-      return <LoginNoStart />; // 완료
+    if (matchingStatus === null) {
+      return <LoginNoStart />; // 매칭신청전!
     }
-    if (state === 2) {
-      return (
-        <LoginWaitMatch teamId={userTeamId?.teamId} status={matchingStatus} />
-      ); // 완료
+    if (matchingStatus === 'APPLIED') {
+      return <LoginWaitMatch teamId={myteamId} />; // 매칭신청완료!
     }
-    if (state === 3) {
-      return (
-        <LoginMatchOk teamId={userTeamId?.teamId} status={matchingStatus} />
-      ); // 완료
+    if (matchingStatus === 'MATCHED') {
+      return <LoginMatchOk />; // 매칭완료!
     }
-    if (state === 4) {
-      return <LoginWaitOtherTeam />;
-    } // 완료
-    if (state === 5) {
-      return <LoginMatchComplete status={matchingStatus} />;
-    } // 완료
-    if (state === 6) {
+    if (matchingStatus === 'OURTEAM_ACCEPTED') {
+      return <LoginWaitOtherTeam />; // 우리팀 수락!
+    }
+    if (matchingStatus === 'SUCCEEDED') {
+      return <LoginMatchComplete />; // 매칭 성공!
+    }
+    if (matchingStatus === 'FAILED') {
+      return <LoginMatchFail teamId={myteamId} />; // 매칭실패!!
+    }
+    if (matchingStatus === 'PARTNER_TEAM_REFUSED') {
       return (
-        <LoginMatchFail teamId={userTeamId?.teamId} status={matchingStatus} />
-      );
-    } // 완료
-    if (state === 7) {
-      return (
-        <LoginOtherTeamRefused
-          teamId={userTeamId?.teamId}
-          status={matchingStatus}
-        />
-      );
-    } // 완료
-    if (state === 8) {
-      return (
-        <LoginNoAnswer teamId={userTeamId?.teamId} status={matchingStatus} />
+        <LoginOtherTeamRefused teamId={myteamId} /> // 상대팀 거절!
       );
     }
-    if (state === 9) {
-      return <LoginRefuse />;
+    if (matchingStatus === 'NOT_RESPONDED') {
+      return <LoginNoAnswer teamId={myteamId} />; // 무응답!
     }
-    if (state === 10) {
-      return <LoginMatchFailed />;
+    if (matchingStatus === 'OURTEAM_REFUSED') {
+      return <LoginRefuse teamId={myteamId} />; // 우리팀 거절!
     }
     return null;
   });
+
+  console.log(myteamId);
+  console.log(matchingStatus);
 
   return (
     <MainLayout>
