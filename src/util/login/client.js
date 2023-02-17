0296aef6ff80191315cae2db9a4658fb0307001e
Darron Park <darron1217@gmail.com>
2023-02-17 14:10:57 +0900

    chore: 백엔드 /api 주소 반영

--- src/util/login/client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create({
-  withCredentials: true,
-  baseURL: process.env.REACT_APP_SERVER_URL,
-  timeout: 1000 * 10,
-});
-
-export default client;
