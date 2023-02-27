69ceec0a1770b5dce398dc6e754090bb3f3b312e
Dongkeun <hongdongk@naver.com>
2023-02-27 11:10:28 +0900

    fix: 채널톡달기

--- src/pages/matching/index.js
+++ src/pages/matching/index.js
@@ -14,6 +14,7 @@ import MainFooter from '../../layout/footer/MainFooter';
 import MainLayout from '../../layout/MainLayout';
 import backend from '../../util/backend';
 import LoginMatchFailed from '../../components/MatchingBox/LoginMatchFailed';
+import ChannelTalk from '../../asset/ChannelTalk';
 
 function Matching() {
   const [myteamId, setMyteamId] = useState('');
@@ -72,6 +73,7 @@ function Matching() {
     <MainLayout>
       {MatchingBox}
       <MainFooter />
+      <div>{ChannelTalk.hideChannelButton()}</div>
     </MainLayout>
   );
 }
