4cef6bc3bef407b945d21417e7e311e7b5ee6398
Dongkeun <hongdongk@naver.com>
2023-02-27 12:11:37 +0900

    fix: 워딩수정 및 디폴트값 변경

--- src/pages/apply/4.js
+++ src/pages/apply/4.js
@@ -56,7 +56,7 @@ function Apply4Page() {
       <NotEnoughIntroModal open={openModal} setModal={setModal} />
       <Title>
         <Maintitle>
-          우리팀을 소개하는 <Pink>마지막 한줄 어필</Pink>
+          우리팀을 소개하는 <Pink>마지막 한 줄 어필</Pink>
         </Maintitle>
         <Subtitle>센스 넘치게 우리 팀을 소개할수록 매칭률이 올라가요!</Subtitle>
         <Subtitle>길게 쓰면 운명의 짝을 만날지도?</Subtitle>
