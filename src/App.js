1e2fff0c9cbeda395242a5eb4d4b519b96807110
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-03-01 19:54:37 +0900

    fix: google analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import { getAnalytics } from 'firebase/analytics';
 import AntdCustomization from './style/AntdCustomization';
 import GlobalStyle from './style/global';
-import app from './config/firebase';
+// eslint-disable-next-line import/named
+import { app } from './config/firebase';
 
 function App() {
-  getAnalytics(app);
-
   return (
     <>
       <GlobalStyle />
