ab003388f2134f9ce14283e24682763db27bdecf
Dongkeun <hongdongk@naver.com>
2023-03-02 19:35:37 +0900

    fix: QA수정

--- src/pages/apply/complete.js
+++ src/pages/apply/complete.js
@@ -49,7 +49,7 @@ function Complete() {
       dispatch(createTeam(applydata));
       window.alert('저장되었습니다!');
     }
-    navigate('/');
+    navigate('/matching');
   });
 
   return (
@@ -107,7 +107,7 @@ const SubmitButton = styled(Button)`
   font-size: 24px;
   text-align: center;
   border: none;
-  margin-top: 10%;
+  margin-top: 25px;
   width: 100%;
   height: 50px;
   background: #eb8888;
