636e05687c417cea6b7c28dae1201c8452fe4e10
MoonGyu1 <s201801402@hufs-gsuite.kr>
2023-03-09 19:13:56 +0900

    fix: 로그아웃 시 apply-data 유지

--- src/features/user/asyncActions.js
+++ src/features/user/asyncActions.js
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import backend from '../../util/backend';
-import { APPLY_STORAGE_KEY } from '../apply';
+import { STORAGE_KEY_ACCESS_TOKEN } from '../../config/constants';
 
 export const logout = createAsyncThunk('user/logout', async () => {
   const response = await backend.get(`/auth/signout`, {
     withCredentials: true,
   });
 
-  // apply 정보 초기화
-  localStorage.removeItem(APPLY_STORAGE_KEY);
+  // acessToken 삭제
+  localStorage.removeItem(STORAGE_KEY_ACCESS_TOKEN);
 
   return response.data;
 });
