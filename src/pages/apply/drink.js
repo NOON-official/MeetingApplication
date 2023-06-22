d7c8856459bb1a43dad5b56e6eb86d694c401a60
kimboyoon <kimboyoon0908@gmail.com>
2023-06-22 23:04:38 +0900

    Add: ApplyKakaoId page

--- src/pages/apply/drink.js
+++ src/pages/apply/drink.js
@@ -63,7 +63,7 @@ export default function ApplyDrink() {
         moreMember: changeCount,
       }),
     );
-    navigate('/');
+    navigate('/apply/kakaoId');
   }, [alchol, changeCount]);
 
   return (
