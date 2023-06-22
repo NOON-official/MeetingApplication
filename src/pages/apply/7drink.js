3be79870ee61c5e0d3bbc80e7ffb6bc75fec86b2
kimboyoon <kimboyoon0908@gmail.com>
2023-06-23 01:34:30 +0900

    fix: area 초기값 변경, area city 분리, apply 파일 순서 정리

--- src/pages/apply/7drink.js
+++ src/pages/apply/7drink.js
@@ -8,149 +8,116 @@ import theme from '../../style/theme';
 import ApplyLayout from '../../layout/ApplyLayout';
 import ApplyButton from '../../components/ApplyButton';
 import ProgressBar from '../../components/ProgressBar';
-import ChooseButton from '../../components/ChooseButton';
-import { submitStep5 } from '../../features/apply';
+import { submitStep7 } from '../../features/apply';
 import IsPageCompleteModal from '../../components/Modal/IsPageCompleteModal';
 import ChannelTalk from '../../asset/ChannelTalk';
+import ChangeCountButton from '../../components/ChangeCountButton';
 
-function Apply5Page() {
+export default function Apply7Page() {
   const [openModal, setOpenModal] = useState(false);
-  const { finishedStep, prefAge, prefVibes } = useSelector(
+  const { finishedStep, drink, memberCount, moreMember } = useSelector(
     (store) => store.apply,
   );
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [alchol, setAlchol] = useState(drink);
+  const [changeCount, setChangeCount] = useState(moreMember);
+
   useEffect(() => {
-    if (finishedStep < 4) {
+    if (finishedStep < 6) {
       window.alert('잘못된 접근입니다');
       navigate(`/apply/${finishedStep + 1}`);
     }
   }, [finishedStep]);
 
-  const marks = {
-    20: { label: <SliderText>20</SliderText> },
-    21: { label: <SliderText>21</SliderText> },
-    22: { label: <SliderText>22</SliderText> },
-    23: { label: <SliderText>23</SliderText> },
-    24: { label: <SliderText>24</SliderText> },
-    25: { label: <SliderText>25</SliderText> },
-    26: { label: <SliderText>26</SliderText> },
-    27: { label: <SliderText>27</SliderText> },
-    28: { label: <SliderText>28</SliderText> },
-    29: { label: <SliderText>29</SliderText> },
+  const marks2 = {
+    1: { label: <SliderText>Level 1</SliderText> },
+    2: { label: <SliderText>2</SliderText> },
+    3: { label: <SliderText>3</SliderText> },
+    4: { label: <SliderText>4</SliderText> },
+    5: { label: <SliderText>5</SliderText> },
   };
 
   const trackStyle = {
     backgroundColor: '#EB8888',
   };
 
-  const [ageRange, setAgeRange] = useState(prefAge.length ? prefAge : [23, 25]);
-  const [prefMood, setPrefMood] = useState(prefVibes);
-
   const setModal = (bool) => {
     setOpenModal(bool);
   };
 
-  const onAfterChange = (value) => {
-    setAgeRange(value);
-  };
-
-  const handleVibe = useCallback(
-    (val, isChecked) => {
-      if (isChecked) {
-        setPrefMood([...prefMood, val]);
-        return;
-      }
-      setPrefMood(prefMood.filter((v) => v !== val));
-    },
-    [prefMood],
-  );
-
   const handleBefore = useCallback(() => {
-    navigate('/apply/4');
+    navigate('/apply/6prefAge');
   });
 
   const handleSubmit = useCallback(() => {
-    if (prefMood.length === 0) {
+    if (!alchol) {
       setOpenModal(true);
       return;
     }
     dispatch(
-      submitStep5({
-        prefAge: ageRange,
-        prefVibes: prefMood,
+      submitStep7({
+        drink: alchol,
+        moreMember: changeCount,
       }),
     );
-    navigate('/apply/drink');
-  }, [ageRange, prefMood]);
-
-  const selectAllMood = () => {
-    if (prefMood.includes(1) && prefMood.includes(2)) {
-      setPrefMood([]);
-      return;
-    }
-    setPrefMood([1, 2]);
-  };
-  const formatter = (value) => `${value}세`;
+    navigate('/apply/8kakaoId');
+  }, [alchol, changeCount]);
 
   return (
     <ApplyLayout>
       <IsPageCompleteModal open={openModal} setModal={setModal} />
       <ProgressBar page={5} />
-      <Title>
-        <Maintitle>
-          <Pink>상대 팀 나이</Pink>를 지정해주세요
-        </Maintitle>
-        <Subtitle>범위를 넓게 선택해야 매칭 확률이 상승해요</Subtitle>
-      </Title>
-      <SSlider
-        onAfterChange={onAfterChange}
-        trackStyle={trackStyle}
-        tooltip={{ formatter, placement: 'bottom' }}
-        marks={marks}
-        defaultValue={ageRange}
-        max={29}
-        min={20}
-        range
-        included
-      />
 
       <Title>
         <Maintitle>
           <Pink>미팅</Pink>은 어땠으면 좋겠어요?
         </Maintitle>
+        <Subtitle>우리 팀의 평균 주량을 알려주세요</Subtitle>
       </Title>
 
-      <ChooseBox2>
-        <ChooseButton
-          isActive={prefMood.includes(1)}
-          onChange={(isActive) => handleVibe(1, isActive)}
-          content="좋은 사람 있으면 소개시켜줘"
-        />
-        <ChooseButton
-          isActive={prefMood.includes(2)}
-          onChange={(isActive) => handleVibe(2, isActive)}
-          content="친구는 다다익선! 찐친 만들어 보자"
-        />
-        <ChooseButton
-          isActive={prefMood.includes(1) && prefMood.includes(2)}
-          onChange={selectAllMood}
-          content="둘 다 좋아요"
-        />
-      </ChooseBox2>
+      <AlcholInfo>
+        <AlcholContent>반 병</AlcholContent>
+        <AlcholContent>한 병</AlcholContent>
+        <AlcholContent>한 병 반</AlcholContent>
+        <AlcholContent>두 병</AlcholContent>
+        <AlcholContent>술고래</AlcholContent>
+      </AlcholInfo>
+      <SSlider
+        onChange={setAlchol}
+        value={alchol}
+        trackStyle={trackStyle}
+        tooltip={{
+          open: false,
+        }}
+        dots
+        marks={marks2}
+        max={5}
+        min={1}
+      />
+
+      <Title>
+        <Maintitle>잠깐, 마지막으로!</Maintitle>
+        <Subtitle>미팅 인원 변경이 가능하다면 모두 체크해주세요</Subtitle>
+      </Title>
+
+      <ChangeCountButton
+        count={memberCount}
+        changeCount={changeCount}
+        setChangeCount={setChangeCount}
+      />
+
       <Footer>
-        <ButtonBox>
+        <ButtonBox2>
           <ApplyButton onClick={handleBefore}>이전</ApplyButton>
           <ApplyButton onClick={handleSubmit}>다음</ApplyButton>
-        </ButtonBox>
+        </ButtonBox2>
       </Footer>
       <div>{ChannelTalk.hideChannelButton()}</div>
     </ApplyLayout>
   );
 }
-
-export default Apply5Page;
 
 const Title = styled.div`
   width: 90%;
@@ -184,7 +151,7 @@ const Footer = styled.div`
   padding-bottom: 5%;
 `;
 
-const ButtonBox = styled.div`
+const ButtonBox2 = styled.div`
   width: 90%;
   display: flex;
   justify-content: center;
@@ -220,8 +187,16 @@ const SliderText = styled.p`
   font-family: 'Nanum JungHagSaeng';
 `;
 
-const ChooseBox2 = styled.div`
-  width: 90%;
+const AlcholInfo = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
+  font-weight: 700;
+  font-size: 14px;
+  margin-top: 8%;
+  width: 90%;
+  color: #eb8888;
+`;
+
+const AlcholContent = styled.div`
+  text-align: center;
 `;
