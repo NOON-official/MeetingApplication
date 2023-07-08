746b1a441162065bbb4948e0c847530c2e6da115
kimboyoon <kimboyoon0908@gmail.com>
2023-07-08 15:56:08 +0900

    add: Area.js, change area type to number

---
+++ src/asset/Area.js
@@ -0,0 +1,18 @@
+const Area = [
+  { id: 1, name: '강남' },
+  { id: 2, name: '건대' },
+  { id: 3, name: '수원' },
+  { id: 4, name: '신촌' },
+  { id: 5, name: '인천' },
+  { id: 6, name: '홍대' },
+  { id: 7, name: '경대 북문' },
+  { id: 8, name: '계대 앞' },
+  { id: 9, name: '동성로' },
+  { id: 10, name: '영대역' },
+  { id: 11, name: '경대 앞' },
+  { id: 12, name: '부산대 앞' },
+  { id: 13, name: '서면' },
+  { id: 14, name: '해운대' },
+];
+
+export default Area;
