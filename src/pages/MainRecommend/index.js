1330dfed9e01e2204ea8d5c677c030eb30771b37
kimboyoon <kimboyoon0908@gmail.com>
2023-07-23 19:32:27 +0900

    add: SCoreDream font woff

--- src/pages/MainRecommend/index.js
+++ src/pages/MainRecommend/index.js
@@ -3,9 +3,6 @@ import { useEffect, useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as MainDoc } from '../../asset/svg/MainDoc.svg';
-import { ReactComponent as MainClock } from '../../asset/svg/MainClock.svg';
-import { ReactComponent as ClockSeparator } from '../../asset/svg/MainClockSeparator.svg';
-import { ReactComponent as UniversityMark } from '../../asset/svg/UniversityMark.svg';
 import { ReactComponent as UniversityMarkPink } from '../../asset/svg/UniversityMarkPink.svg';
 import { ReactComponent as MainGroup } from '../../asset/svg/MainGroup.svg';
 import backend from '../../util/backend';
@@ -35,8 +32,6 @@ export default function Recommend() {
   useEffect(() => {
     getInformation();
   }, []);
-
-  console.log(teamProfile);
 
   return (
     <>
@@ -157,114 +152,4 @@ const Subtitle = styled.div`
   font-size: 14px;
   font-weight: 300;
   line-height: 18px;
-`;
-
-const Content = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 14px;
-  font-weight: 500;
-`;
-
-const SMainClock = styled(MainClock)`
-  margin: 7px;
-`;
-
-const Clock = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  width: 100px;
-`;
-
-const Box = styled.div`
-  width: 17px;
-  height: 17px;
-  padding: 6px 4px 3px 4px;
-  border-radius: 5px;
-  color: #eb8888;
-  background-color: #ffd8d8;
-`;
-
-const SSeparator = styled(ClockSeparator)`
-  margin: 0 2px;
-`;
-
-const Container2 = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  width: 90%;
-  margin: 5% auto;
-`;
-
-const TeamCard = styled.div`
-  border: 1px solid #d74683;
-  border-radius: 6px;
-  background-color: #ffffff;
-  width: 40%;
-  margin: 2% 0;
-  padding: 4%;
-`;
-
-const Title2 = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const TeamName = styled.span`
-  background-color: #ececec;
-  padding: 5px 8px;
-  font-weight: 600;
-  font-size: 13px;
-  border-radius: 2px;
-  overflow: hidden;
-  white-space: nowrap;
-  word-break: break-all;
-  text-overflow: ellipsis;
-`;
-
-const SUniversityMark = styled(UniversityMark)`
-  margin-left: 1%;
-`;
-
-const Subtitle2 = styled.div`
-  display: flex;
-  justify-content: space-between;
-  color: #626262;
-  font-size: 10px;
-  width: 50%;
-  margin: 5% 10%;
-`;
-
-const Age = styled.div``;
-
-const MemberCount = styled.div``;
-
-const Info = styled.div`
-  margin-top: 5%;
-  font-size: 8px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: normal;
-  line-height: 1.3;
-  text-align: left;
-  word-wrap: break-word;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-`;
-
-const Button = styled.div`
-  padding: 5px 15px;
-  width: 37%;
-  margin: 7% auto 0;
-  border-radius: 15px;
-  color: #ffffff;
-  background-color: #ff7ab2;
-  font-size: 10px;
-  font-weight: 500;
-  text-align: center;
 `;
