25d353060e5d1d52b9ac30f93cf2ba97b150df1a
kimboyoon <kimboyoon0908@gmail.com>
2023-08-03 02:30:03 +0900

    add: myinfo myaccount UI

--- src/pages/myinfo/account/index.js
+++ src/pages/myinfo/account/index.js
@@ -6,21 +6,19 @@ import styled from 'styled-components';
 import PrimaryModal from '../../../components/Modal/PrimaryModal';
 import PrimaryButton from '../../../components/PrimaryButton';
 import Section from '../../../components/Section';
-import {
-  useGetMyInfoQuery,
-  useGetUserTeamsQuery,
-} from '../../../features/backendApi';
+import { useGetMyInfoQuery } from '../../../features/backendApi';
 import { logout } from '../../../features/user/asyncActions';
 import MyinfoLayout from '../../../layout/MyinfoLayout';
 import backend from '../../../util/backend';
+import Universities from '../../../asset/Universities';
+import { ReactComponent as UniversityMark } from '../../../asset/svg/UniversityMark.svg';
 
 export default function Account() {
   const [resignModalOpened, setResignModalOpened] = useState(false);
   const { data: myInfo } = useGetMyInfoQuery();
-  const { data: teamsData } = useGetUserTeamsQuery();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  console.log(myInfo);
+
   const deleteAccount = useCallback(async () => {
     try {
       await backend.delete('/auth/account', {
@@ -36,65 +34,41 @@ export default function Account() {
   });
 
   return (
-    <MyinfoLayout title="계정 관리">
-      <Section my="12px">
-        <InfoCard>
-          <Subtitle>내 정보</Subtitle>
-          <Row>
-            <Col span={4}>이름</Col>
-            <Col span={20}>{myInfo?.nickname}</Col>
-          </Row>
-          <Row>
-            <Col span={4}>성별</Col>
-            <Col span={20}>{myInfo?.gender === 'male' ? '남성' : '여성'}</Col>
-          </Row>
-          <Row>
-            <Col span={4}>대학교</Col>
-            {/* <Col span={20}>{Universities.{myInfo?.university -1}}</Col> */}
-          </Row>
-          <Row>
-            <Col span={4}>전화번호</Col>
-            <Col span={20}>
-              {myInfo?.phone}{' '}
-              <PhoneChangeButton
-                onClick={() => navigate('/myinfo/account/phone')}
-              >
-                변경
-              </PhoneChangeButton>
-            </Col>
-          </Row>
-        </InfoCard>
-      </Section>
-      {/* <Section>
-        <Accordion
-          title={<AccordionTitle>신청 내역</AccordionTitle>}
-          content={
-            <HistoryContainer>
-              <HistoryListText>
-                {myInfo?.nickname} 님은 지금까지{' '}
-                <span>{teamsData?.teams.length}회</span> 미팅 신청했어요!
-              </HistoryListText>
-              <HistoryList>
-                {teamsData?.teams.map((team) => (
-                  <HistoryItem key={team.id}>
-                    <HistoryTitleText>
-                      {MEMBER_COUNT_LABELS[team.memberCount]} 미팅
-                    </HistoryTitleText>
-                    <div>
-                      <HistoryDateText>
-                        {dayjs(team.createdAt).format('YYYY. MM. DD HH:mmA')}{' '}
-                      </HistoryDateText>
-                      <HistoryStatusText>
-                        {team.chatCreatedAt ? '매칭 완료' : '신청 완료'}
-                      </HistoryStatusText>
-                    </div>
-                  </HistoryItem>
-                ))}
-              </HistoryList>
-            </HistoryContainer>
-          }
-        />
-      </Section> */}
+    <MyinfoLayout title="내 정보">
+      {myInfo && (
+        <Section my="12px">
+          <InfoCard>
+            <Subtitle>내 정보</Subtitle>
+            <Row>
+              <Col span={4}>이름</Col>
+              <Col span={15}>{myInfo.nickname}</Col>
+            </Row>
+            <Row>
+              <Col span={4}>성별</Col>
+              <Col span={15}>{myInfo.gender === 'male' ? '남성' : '여성'}</Col>
+            </Row>
+            <Row>
+              <Col span={5}>대학교</Col>
+              <Col span={18}>
+                {Universities[myInfo.university - 1]?.name}
+                <SUniversityMarkPink />
+                <Mark>학교 인증 완료</Mark>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={6}>전화번호</Col>
+              <Col span={15}>
+                {myInfo.phone}
+                <PhoneChangeButton
+                  onClick={() => navigate('/myinfo/account/phone')}
+                >
+                  변경
+                </PhoneChangeButton>
+              </Col>
+            </Row>
+          </InfoCard>
+        </Section>
+      )}
       <Section my="8px" style={{ textAlign: 'right' }}>
         <ResignButton type="text" onClick={() => setResignModalOpened(true)}>
           회원 탈퇴
@@ -122,87 +96,53 @@ export default function Account() {
 }
 
 const Subtitle = styled.div`
-  font-size: 14px;
-  color: ${(props) => props.theme.grey};
-  padding-left: 6px;
-  padding-bottom: 5px;
+  margin-bottom: 5%;
+  font-size: 16px;
+  color: ${(props) => props.theme.black};
+  font-weight: 600;
 `;
 
 const InfoCard = styled(Card)`
+  .ant-row {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 5%;
+  }
+
   .ant-col {
-    font-family: 'Nanum JungHagSaeng';
-    font-size: 18px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
 
     :first-child {
-      color: #bbbbbb;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 10px;
+      color: #eb8888;
+      background-color: #ffeded;
+      font-size: 15px;
+      text-align: center;
     }
+
     :last-child {
+      padding-left: 20px;
       color: #777777;
+      font-size: 14px;
     }
   }
 `;
 
-const AccordionTitle = styled.span`
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 17px;
-  color: #777777;
+const SUniversityMarkPink = styled(UniversityMark)`
+  margin: 0 5px 0 15px;
+  width: 18px;
 `;
 
-const HistoryContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-`;
-
-const HistoryList = styled.div`
-  display: flex;
-  gap: 7px;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const HistoryListText = styled.span`
-  font-family: 'Nanum JungHagSaeng';
+const Mark = styled.div`
+  color: #e8a4c0;
   font-weight: 400;
-  font-size: 24px;
-  line-height: 28px;
-  > span {
-    color: #eb8888;
-  }
-`;
-
-const HistoryItem = styled.div`
-  background: #fff5f5;
-  border: 1px solid #eb8888;
-  border-radius: 3px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px 30px;
-`;
-
-const HistoryTitleText = styled.span`
-  color: #777777;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 16px;
-`;
-
-const HistoryDateText = styled.span`
-  color: #777777;
-  font-weight: 300;
-  font-size: 10px;
-  line-height: 12px;
-`;
-
-const HistoryStatusText = styled.span`
-  color: #eb8888;
-  font-weight: 300;
-  font-size: 10px;
-  line-height: 12px;
-  margin-left: 4px;
+  font-size: 12px;
 `;
 
 const ResignButton = styled(Button)`
@@ -211,18 +151,13 @@ const ResignButton = styled(Button)`
   }
 `;
 
-const PhoneChangeButton = styled(Button).attrs({ type: 'text' })`
-  height: auto;
-  font-family: 'Nanum JungHagSaeng';
-  padding: 4px 12px;
-  border-radius: 10px;
-  background-color: ${(props) => props.theme.pink};
+const PhoneChangeButton = styled.button`
+  margin-left: 7%;
+  padding: 5px 8px;
+  border-radius: 5px;
+  background-color: #e6e6e6;
   border: none;
-  box-shadow: none;
-
-  > span {
-    font-weight: 400;
-    font-size: 13px;
-    color: white;
-  }
+  font-weight: 400;
+  font-size: 12px;
+  color: #777777;
 `;
