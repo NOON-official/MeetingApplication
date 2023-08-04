1ab6818be08504aecc20832207e2c77afc3fb2d9
kimboyoon <kimboyoon0908@gmail.com>
2023-08-04 17:52:18 +0900

    add: ting UI, student card approval column 추가하여 수정

--- src/layout/MyinfoLayout.js
+++ src/layout/MyinfoLayout.js
@@ -33,7 +33,9 @@ const Content = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${(props) =>
-    props.title === '학교 인증' || props.title === '보유 팅'
+    props.title === '학교 인증' ||
+    props.title === '보유 팅' ||
+    props.title === '팅 충전하기'
       ? props.theme.white
       : props.theme.background};
   overflow-y: scroll;
