49a1ea73fa5d7e6c021306fb4d2ebe00c8ceccfa
kimboyoon <kimboyoon0908@gmail.com>
2023-08-09 16:38:00 +0900

    add: 학생증 인증안했을 때 분기처리, 회원가입시 무료 팅 모달

---
+++ src/components/Modal/FreeTingModal.js
@@ -0,0 +1,115 @@
+import { Modal } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Mailbox } from '../../asset/svg/Mailbox.svg';
+import { useGetMyInfoQuery } from '../../features/backendApi';
+
+export default function FreeTingModal({ open, setModal }) {
+  const navigate = useNavigate();
+  const { data: myinfo } = useGetMyInfoQuery();
+
+  return (
+    <div>
+      {open ? (
+        <SModal
+          open={open}
+          footer={null}
+          centered
+          width="380px"
+          closable
+          onCancel={() => {
+            setModal(false);
+            navigate('/apply/invite');
+          }}
+        >
+          <Container>
+            <Header>회원가입을 축하해요!</Header>
+            <TextBox>
+              <BlackText>
+                <ColorText>
+                  무료 {myinfo?.gender === 'female' ? 20 : 6}팅
+                </ColorText>
+                이 도착했어요!
+              </BlackText>
+              <Content>
+                <SmallText>
+                  우리 팀 프로필을 만든 후<br />
+                  사용해보세요!
+                </SmallText>
+                <SMailbox />
+              </Content>
+            </TextBox>
+          </Container>
+        </SModal>
+      ) : null}
+    </div>
+  );
+}
+
+const SModal = styled(Modal)`
+  .ant-modal-content {
+    padding-bottom: 0;
+    padding-right: 0;
+    background-color: #fbfaf9;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+`;
+
+const TextBox = styled.div`
+  /* position: relative; */
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Header = styled.span`
+  display: inline;
+  border-radius: 15px;
+  padding: 1% 6%;
+  margin-bottom: 5%;
+  color: #ffffff;
+  background-color: #000;
+  font-family: 'GmarketSans';
+  font-size: 14px;
+`;
+
+const BlackText = styled.span`
+  color: #000000;
+  font-size: 25px;
+  font-family: 'PyeongChangPeace';
+  line-height: 28px;
+`;
+
+const ColorText = styled.span`
+  background: linear-gradient(
+    90deg,
+    #a143ff 0%,
+    #59f 30.73%,
+    #4fffea 65.63%,
+    #52ff63 100%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const SmallText = styled.span`
+  font-size: 14px;
+  color: #525252;
+  font-family: 'GmarketSans';
+`;
+
+const SMailbox = styled(Mailbox)``;
--- src/layout/header/MainMatchingHeader.js
+++ src/layout/header/MainMatchingHeader.js
@@ -3,13 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { ReactComponent as MainDoc } from '../../asset/svg/MainDoc.svg';
 import { ReactComponent as UniversityMarkDarkGray } from '../../asset/svg/UniversityMarkDarkGray.svg';
+import { ReactComponent as UniversityMarkPink } from '../../asset/svg/UniversityMark.svg';
 import MyTeamProfileModal from '../../components/MainRecommend/MyTeamProfileModal';
 import { ReactComponent as MainGroup } from '../../asset/svg/MainGroup.svg';
 import { ReactComponent as TingImg } from '../../asset/svg/TingImg.svg';
-import { useGetUserTingCountQuery } from '../../features/backendApi';
+import {
+  useGetMyInfoQuery,
+  useGetUserTingCountQuery,
+} from '../../features/backendApi';
 
 export default function MainMatchingHeader({ title }) {
   const { data: ting } = useGetUserTingCountQuery();
+  const { data: myinfo } = useGetMyInfoQuery();
   const [openMyTeamProfile, setOpenMyTeamProfile] = useState(false);
   const navigate = useNavigate();
 
@@ -31,13 +36,22 @@ export default function MainMatchingHeader({ title }) {
           {title === '프로필 조회' ? <SMainDoc /> : <SMainGroup />}
           <BtnMainTitle>{title}</BtnMainTitle>
         </MainButton>
-        <MainButton>
-          <SUniversityMarkDarkGray />
-          <BtnTitle onClick={() => navigate('/myinfo/studentcard')}>
-            <BtnMainTitle>학교 인증하기</BtnMainTitle>
-            <BtnSubtitle>학교 인증 전</BtnSubtitle>
-          </BtnTitle>
-        </MainButton>
+        {myinfo?.approval === 1 ? (
+          <MainButton>
+            <UniversityMarkPink />
+            <BtnTitle>
+              <BtnMainTitlePink>학교 인증 완료</BtnMainTitlePink>
+            </BtnTitle>
+          </MainButton>
+        ) : (
+          <MainButton>
+            <SUniversityMarkDarkGray />
+            <BtnTitle onClick={() => navigate('/myinfo/studentcard')}>
+              <BtnMainTitle>학교 인증하기</BtnMainTitle>
+              <BtnSubtitle>학교 인증 전</BtnSubtitle>
+            </BtnTitle>
+          </MainButton>
+        )}
       </ButtonBox>
       <MainButton2 onClick={() => navigate('/myinfo/ting')}>
         <STingImg />
@@ -98,6 +112,14 @@ const BtnTitle = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
+`;
+
+const BtnMainTitlePink = styled.div`
+  margin-left: 15px;
+  background: linear-gradient(90deg, #ccb5f3 0%, #ff9595 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 `;
 
 const BtnMainTitle = styled.div`
