cceda334378e1c3efbd33baa5c708884cc015ab4
kimboyoon <kimboyoon0908@gmail.com>
2023-09-06 16:09:45 +0900

    fix: add not enough ting modal, accept matching, refuse matching, change font family and font size

--- src/pages/matching/NoMatching.js
+++ src/pages/matching/NoMatching.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as NoMatchingData } from '../../asset/svg/NoMatchingData.svg';
 
 export default function NoMatching({ children }) {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Title>{children}</Title>
       <NoMatchingData />
+      <Button onClick={() => navigate('/')}>
+        <CreateTeamBtn>신청하러 가기</CreateTeamBtn>
+      </Button>
     </Container>
   );
 }
@@ -25,4 +31,20 @@ const Title = styled.div`
   color: #9b9b9b;
   font-size: 15px;
   font-weight: 500;
+`;
+
+const Button = styled.div`
+  width: 30%;
+`;
+
+const CreateTeamBtn = styled.button`
+  width: 100%;
+  margin-top: 10%;
+  padding: 7px 5px;
+  border: none;
+  border-radius: 20px;
+  background-color: #ffcdcd;
+  color: #eb8888;
+  font-size: 14px;
+  font-weight: 600;
 `;
