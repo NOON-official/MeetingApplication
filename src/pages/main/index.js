d537a75244b0e76b20432a45a8ae1495c74329d9
kimboyoon <kimboyoon0908@gmail.com>
2023-09-15 14:51:52 +0900

    add: service err modal

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,11 @@
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { Space } from 'antd';
 import MainLayout from '../../layout/MainLayout';
 import Home from '../home';
 import MeetingIntro from './MeetingIntro';
+import PrimaryModal from '../../components/Modal/PrimaryModal';
+import { ReactComponent as Alert } from '../../asset/svg/ExclamationMark.svg';
 
 function Main() {
   const params = new URLSearchParams(window.location.search);
@@ -16,7 +19,35 @@ function Main() {
     }
   }, [referralId]);
 
-  return <MainLayout>{accessToken ? <Home /> : <MeetingIntro />}</MainLayout>;
+  return (
+    <MainLayout>
+      <PrimaryModal title=" " open footer={null} closeIcon={false}>
+        <Space
+          direction="vertical"
+          style={{
+            textAlign: 'center',
+            width: '100%',
+            backgroundColor: '#fff',
+          }}
+        >
+          <Alert />
+          <span style={{ fontSize: '18px', fontWeight: '600' }}>
+            일시적으로 서비스 이용이 불가합니다.
+          </span>
+          <span style={{ fontSize: '16px' }}>
+            업데이트 및 점검 중입니다.
+            <br />
+            완료 후 문자를 발송해드릴 예정이니
+            <br />
+            조금만 더 기다려 주세요!
+            <br />
+            이용에 불편을 드려 죄송합니다.
+          </span>
+        </Space>
+      </PrimaryModal>
+      {accessToken ? <Home /> : <MeetingIntro />}
+    </MainLayout>
+  );
 }
 
 export default Main;
